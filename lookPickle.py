aeacba93f04975d4a4544de5ac23896514ecdfb8
holuca <lucahollenstein4@gmail.com>
2025-01-23 09:43:46 +0100

    cleaener

--- lookPickle.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-# Open the pickle file in read-binary mode
-with open('./data/chest_50.pickle', 'rb') as file:
-    data = pickle.load(file)
-
-# Now 'data' contains the deserialized Python object
-print(data)
